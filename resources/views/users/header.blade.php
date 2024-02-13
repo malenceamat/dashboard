6fa0387295b4d2027084389226904e68c68aa84c
malenceamat <yatikivan@gmail.com>
2024-02-13 18:06:05 +0300

    замена лого в хедере

--- resources/views/users/header.blade.php
+++ resources/views/users/header.blade.php
@@ -4,9 +4,9 @@
             <div class="header-logo header-logo-ls logo animated" data-animate="fadeInDown" data-delay=".65">
                 <a href="" class="logo-link">
                     <img class="logo-dark"
-                         src={{asset("images/header.png")}} srcset="images/header.png" alt="logo">
+                         src={{asset("images/logo.png")}} srcset={{asset("images/logo.png")}} alt="logo">
                     <img class="logo-light"
-                         src={{asset("images/header.png")}} srcset="images/header.png" alt="logo">
+                         src={{asset("images/logo.png")}} srcset={{asset("images/logo.png")}} alt="logo">
                 </a>
             </div>
         </div>
