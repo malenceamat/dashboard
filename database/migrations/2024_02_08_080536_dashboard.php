909be658653f0418dfcace6b0cc17340e656837d
malenceamat <yatikivan@gmail.com>
2024-02-14 17:25:51 +0300

    добавление новый строк в таблицу (на главной странице и на админке)+

--- database/migrations/2024_02_08_080536_dashboard.php
+++ database/migrations/2024_02_08_080536_dashboard.php
@@ -16,11 +16,14 @@ class Dashboard extends Migration
         Schema::create('dashboard', function (Blueprint $table) {
             $table->id();
             $table->longText('name')->nullable();
-            $table->longText('base')->nullable();
-            $table->longText('special')->nullable();
-            $table->longText('result')->nullable();
-            $table->longText('percent')->nullable();
+            $table->longText('base_plan')->nullable();
+            $table->longText('base_percent')->nullable();
+            $table->longText('base_fact')->nullable();
+            $table->longText('spec_plan')->nullable();
+            $table->longText('spec_fact')->nullable();
+            $table->longText('spec_percent')->nullable();
             $table->longText('sub_name')->nullable();
+            $table->longText('result')->nullable();
             $table->timestamps();
         });
     }
